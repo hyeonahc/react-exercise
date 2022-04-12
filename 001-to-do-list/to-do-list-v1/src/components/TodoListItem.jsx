ea5307a62495500cc2017714ea4b2e6e089a65ab
hyeonah <hyeonah.hello@gmail.com>
2022-04-12 15:06:29 +0900

    Rename directory

---
+++ 001-to-do-list/to-do-list-v1/src/components/TodoListItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './TodoListItem.scss';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames';
+
+const TodoListItem = ({ todo, onToggle, onRemove }) => {
+  const { id, value, checked } = todo;
+
+  return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{value}</div>
+      </div>
+      <div className="remove" checked={checked} onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;

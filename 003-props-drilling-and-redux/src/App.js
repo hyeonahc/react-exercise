ca20db64429009e8713eda70bacc5c25cfa9c6b2
hyeonah <hyeonah.hello@gmail.com>
2022-04-14 15:37:43 +0900

    tutorial: Learn props drilling issue and redux

---
+++ 003-props-drilling-and-redux/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropsDrilling from './PropsDrilling';
+import Redux from './Redux';
+
+const App = () => {
+  // return <PropsDrilling />;
+  return <Redux />;
+};
+
+export default App;
